22ca54c5e937a188912b432bd0c476da8f97277d
HP <HP@DESKTOP-C0RB8R0>
2024-03-27 16:11:46 +0630

    Add project files.

---
+++ DotNetCore.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int i;
+        for (i = 0; i <= 10; i++)
+        {
+            if (i == 5)
+                continue;
+            if (i == 8)
+                break;
+            Console.WriteLine("Value is " + i);
+        }
+        Console.ReadLine();
+    }
+}
+
+
